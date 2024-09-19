b5ea2fed09d442465c887cc998ed55946e567a4c
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-09-19 20:34:59 +0900

    [level 3] Title: 네트워크, Time: 10.00 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,33 +1,44 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 import java.lang.*;
 
 class Solution {
     public int[] visited;
-    public int[][] com;
-    public int network=0;
+    public int nat=0;
     
     public int solution(int n, int[][] computers) {
-        com = computers;
-        visited= new int[n];
+        visited = new int[n];
         
-    
         for(int i=0;i<n;i++){
             if(visited[i]==0){
-                dfs(i,n);
-                network++;
+                nat++;
+                bfs(computers,i,n);
             }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(visited[i]+" ");
         }
         
-        return network;
+        return nat;
     }
     
-    public void dfs(int cur, int n){
-        visited[cur]=1;
-        for(int i=0;i<n;i++){
-            if(com[cur][i]==1 && visited[i]==0){
-                dfs(i,n);
+    public void bfs(int[][]com,int start, int n){
+        Queue<Integer>que = new LinkedList<>();
+        que.offer(start);
+        visited[start]=nat;
+        
+        while(!que.isEmpty()){
+            int now = que.poll();
+            for(int i=0;i<n;i++){
+                if(com[now][i]==1){
+                    if(visited[i]==0){
+                        visited[i]=visited[now];
+                        que.offer(i);
+                    }
+                }
+                
             }
         }
+ 
     }
 }
