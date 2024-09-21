3f5f0379075e3b10c070f768a6082ab08ca99249
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-09-21 09:22:42 +0900

    [level 3] Title: 아이템 줍기, Time: 0.86 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,32 +1,33 @@
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
 class Solution {
     public int[][] map;
-    public int n;
-    public int[] dx = {1,-1,0,0};
-    public int[] dy = {0,0,1,-1};
     public int[][] visited;
+    public int[] dx = {0,0,1,-1};
+    public int[] dy = {1,-1,0,0};
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        n = rectangle.length;
+        int answer = 0;
+        
         map = new int[102][102];
         visited = new int[102][102];
         
-        for(int[]rect : rectangle){
+        for(int[] rect : rectangle){
             int x1 = rect[0]*2;
             int y1 = rect[1]*2;
             int x2 = rect[2]*2;
             int y2 = rect[3]*2;
             
-            for(int x=x1;x<=x2;x++){
-                if(map[y1][x]!=2) map[y1][x]=1;
-                if(map[y2][x]!=2) map[y2][x]=1;
+            for(int i=x1;i<=x2;i++){
+                if(map[y1][i]!=2) map[y1][i]=1;
+                if(map[y2][i]!=2) map[y2][i]=1;
             }
-            for(int y=y1;y<y2;y++){
-                if(map[y][x1]!=2) map[y][x1]=1;
-                if(map[y][x2]!=2) map[y][x2]=1;
+            
+            for(int i=y1;i<=y2;i++){
+                if(map[i][x1]!=2) map[i][x1]=1;
+                if(map[i][x2]!=2) map[i][x2]=1;
             }
             
             for(int i=x1+1;i<x2;i++){
@@ -36,10 +37,10 @@ class Solution {
             }
         }
         
-        
-        return bfs(characterX*2, characterY*2, itemX*2,itemY*2)/2;
+        return bfs(characterX*2, characterY*2, itemX*2, itemY*2)/2;
     }
-    public int bfs(int sx, int sy, int endx, int endy){
+    
+    public int bfs(int sx, int sy, int tx, int ty){
         Queue<int[]> que = new LinkedList<>();
         que.offer(new int[]{sx,sy});
         visited[sy][sx]=1;
@@ -49,22 +50,19 @@ class Solution {
             int x = now[0];
             int y = now[1];
             
-            if(x==endx && y==endy){
-                return visited[endy][endx];
+            if(x==tx && y==ty){
+                return visited[y][x];
             }
             
             for(int i=0;i<4;i++){
                 int nx = x+dx[i];
                 int ny = y+dy[i];
-                
-                if(nx>=1 && nx<=102 && ny>=1 && ny<=102 && map[ny][nx]==1){
-                    if(visited[ny][nx]==0){
-                        visited[ny][nx]=visited[y][x]+1;
-                        que.offer(new int[]{nx,ny});
-                    }
+                if(nx>0&&nx<102 && ny>0 && ny<102 && map[ny][nx]==1 &&visited[ny][nx]==0){
+                    visited[ny][nx]=visited[y][x]+1;
+                    que.offer(new int[]{nx,ny});
                 }
             }
         }
-        return -1;
+        return 0;
     }
 }
