1c9af772f83e7358345b37c7d914084fdcd253e1
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-09-25 16:31:07 +0900

    [level 3] Title: 여행경로, Time: 0.53 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,53 +1,50 @@
 import java.util.*;
-import java.io.*;
-import java.lang.*;
 
 class Solution {
     public boolean[] visited;
-    public ArrayList<String> arr = new ArrayList<>();
-    public ArrayList<String> answer = new ArrayList<>();
+    public int n;
+    public ArrayList<String> route;
     
     public String[] solution(String[][] tickets) {
-        visited = new boolean[tickets.length];
-        
         Arrays.sort(tickets, new Comparator<String[]>(){
             @Override
             public int compare(String[] o1, String[] o2){
                 if(o1[0].equals(o2[0])){
                     return o1[1].compareTo(o2[1]);
-                }
+                }     
                 return o1[0].compareTo(o2[0]);
             }
         });
         
-        arr.add("ICN");  
-        dfs("ICN", tickets, 0);
+        n=tickets.length;
+        visited = new boolean[n];
+        route = new ArrayList<>();
         
-        String[] result = new String[answer.size()];
+        route.add("ICN");
+        dfs("ICN", tickets);
         
-        for (int i = 0; i < answer.size(); i++) {
-            result[i] = answer.get(i);
-        }
-        
-        return result;
+        return route.toArray(new String[0]);
     }
-    public boolean dfs(String from, String[][] tickets, int count) {
-        if (count == tickets.length) {
-            answer = new ArrayList<>(arr);
-            return true; 
+    
+     private boolean dfs(String current, String[][] tickets) {
+        if (route.size() == n + 1) {
+            return true;
         }
         
-        for (int i = 0; i < tickets.length; i++) {
-            if (!visited[i] && tickets[i][0].equals(from)) {
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && tickets[i][0].equals(current)) {
                 visited[i] = true;
-                arr.add(tickets[i][1]);
-                if (dfs(tickets[i][1], tickets, count + 1)) {
+                route.add(tickets[i][1]);
+                
+                if (dfs(tickets[i][1], tickets)) {
                     return true;
                 }
-                arr.remove(arr.size() - 1); 
-                visited[i] = false; 
+                
+                visited[i] = false;
+                route.remove(route.size() - 1);
             }
         }
+        
         return false;
     }
 }
