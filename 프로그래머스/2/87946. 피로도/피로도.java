c44cce6698c11f2805dadbfd2760c170a0241f01
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-09-26 02:13:40 +0900

    [level 2] Title: 피로도, Time: 2.96 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,27 +1,29 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 class Solution {
-    public int n;
     public int[] visited;
-    public int maxDun=0;
+    public int n;
+    public int cnt=1;
+    public int max=0;
     
     public int solution(int k, int[][] dungeons) {
-        visited = new int[dungeons.length];
-        back(k,dungeons,0);
+        n = dungeons.length;
+        visited = new int[n];
+        dfs(dungeons,k,0);
         
-        return maxDun;
+        return max;
     }
-    public void back(int cur, int[][] dun, int count){
-        if(count > maxDun){
-            maxDun = count;
+    public void dfs(int[][] dun, int cur,int cnt){
+        if(cnt>max){
+            max=cnt;
         }
-        for(int i=0;i<dun.length;i++){
-            if(visited[i]==0 && cur>=dun[i][0]){
-                visited[i]=1;
-                back(cur-dun[i][1],dun,count+1);
-                visited[i]=0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==0){
+                if(cur>=dun[i][0]){
+                    visited[i]=1;
+                    dfs(dun,cur-dun[i][1],cnt+1);
+                    visited[i]=0;
+                }
             }
         }
     }
