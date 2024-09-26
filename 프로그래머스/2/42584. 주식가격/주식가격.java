4f27532ea887e0fd1c5ca32b19c987bc7bb9f7dd
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-09-26 14:54:28 +0900

    [level 2] Title: 주식가격, Time: 46.55 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,27 +1,23 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
-        int[] result=new int[n];
+        int[] answer = new int[n];
         
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> st = new Stack<>();
         
         for(int i=0;i<n;i++){
-            while(!stack.isEmpty() && prices[i]<prices[stack.peek()]){
-                int topIndex = stack.pop();
-                result[topIndex]=i-topIndex;
+            while(!st.isEmpty() && prices[i]<prices[st.peek()]){
+                answer[st.peek()]=i-st.pop();
             }
-            stack.push(i);
+            st.push(i);
+        }
+        int l = st.size();
+        for(int i=0;i<l;i++){
+            answer[st.peek()]=n-1-st.pop();
         }
         
-        while(!stack.isEmpty()){
-            int topIndex = stack.pop();
-            result[topIndex] = n-topIndex-1;
-        }
-        
-        return result;
+        return answer;
     }
 }
