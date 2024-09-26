c57bf5ec766ba1d78cb526280493e8c4695d75a0
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-09-26 10:11:05 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,24 +1,23 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        for(int i=0;i<commands.length;i++){
+        int n = commands.length;
+        int[] answer = new int[n];
+        
+        for(int i=0;i<n;i++){
             int s = commands[i][0]-1;
             int e = commands[i][1]-1;
-            int k = commands[i][2]-1;
             
-            int[] arr = new int[e-s+1];
-            int cnt=0;
-            for(int j=s;j<=e;j++){
-                arr[cnt]=array[j];
-                cnt++;
+            int len = e-s+1;
+            int[] ch = new int[len];
+            for(int j=0;j<len;j++){
+                ch[j]=array[s+j];
             }
-            Arrays.sort(arr);
-            answer[i]=arr[k];
+            Arrays.sort(ch);
+            answer[i]=ch[commands[i][2]-1];
         }
+        
         return answer;
     }
 }
