fe490997344d9482544ae28e1c1b4da2e3575068
soeun kim <113100154+soeun1230@users.noreply.github.com>
2024-09-28 16:48:17 +0900

    [level 2] Title: 타겟 넘버, Time: 5.92 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,15 +1,29 @@
 import java.util.*;
 
 class Solution {
+    public int n;
+    public int[] visited;
+    public int[] num;
+    public int cnt=0;
+    
     public int solution(int[] numbers, int target) {
         int answer = 0;
-        return dfs(0,0,numbers,target);
+        n = numbers.length;
+        visited = new int[n];
+        num = numbers;
+        
+        int cnt =dfs(0,0,target);
+        
+        return cnt;
     }
-    public int dfs(int depth,int cur, int[] num, int tar){
-        if(depth==num.length){
-            if(cur==tar) return 1;
+    public int dfs(int dep, int cur, int tar){
+        if(dep==n){
+            if(cur==tar){
+                return 1;
+            }
             return 0;
         }
-        return dfs(depth+1,cur+num[depth],num,tar)+dfs(depth+1,cur-num[depth],num,tar);
+
+        return dfs(dep+1,cur+num[dep],tar) + dfs(dep+1, cur-num[dep],tar);
     }
 }
